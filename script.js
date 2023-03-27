a02081228ff9fda90d6fc7040c5d3351be76ca0e
Daniel J <68441865+djewel23@users.noreply.github.com>
2023-03-27 13:56:08 -0600

    Declaring variables to be used.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.overlay');
+const closeModalBtn =  document.querySelector('.close-modal');
+const openModalBtns = document.querySelectorAll('.show-modal');
+
+for( let i = 0; i < openModalBtns.length; i++){
+  console.log(openModalBtns[i].textContent);
+}
+
