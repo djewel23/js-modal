a65fb2c51f4b804b4e65f7e73fc1d78be0bd2019
Daniel J <68441865+djewel23@users.noreply.github.com>
2023-03-27 14:25:55 -0600

    Added in event listeners and functions for said listeners

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 'use strict';
 
+// Declared variables
 const modalWindow = document.querySelector('.modal');
 const modalOverlay = document.querySelector('.overlay');
 const closeModalBtn =  document.querySelector('.close-modal');
 const openModalBtns = document.querySelectorAll('.show-modal');
 
+// Open modal function
+const openModal = function(){
+  modalWindow.classList.remove('hidden');
+  modalOverlay.classList.remove('hidden');
+};
+
+// Close modal function
+const closeModal = function(){
+  modalWindow.classList.add('hidden');
+  modalOverlay.classList.add('hidden');
+};
+
+// For Loop to cycle through each of the "open" buttons. Includes event listener.
 for( let i = 0; i < openModalBtns.length; i++){
-  console.log(openModalBtns[i].textContent);
+  openModalBtns[i].addEventListener('click', openModal);
 }
 
+// Hide modal window when "X" is clicked
+closeModalBtn.addEventListener('click', closeModal);
+
+// Hide modal window when click occurs on overlay
+modalOverlay.addEventListener('click', closeModal);
